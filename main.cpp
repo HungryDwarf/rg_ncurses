a5fb22e918d4425b0d1ebd8dd4709ba6b0d36edd
HungryDwarf <q@localhost.localdomain>
2016-05-12 00:17:07 +0300

    travis again

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@
 int main(){
 	initscr();
 	curs_set(0);
+	noecho();
 
-	printw("TEST");
-	
 	Character ch;
 	Character::Controller cont;
 	int x = ch.getX();
 	int y = ch.getY();
 	mvprintw(y, x, "@");
-	
+
 	while(cont.exit == 0){
 		int gc = getch();
 		cont.move(gc);
-	}	
-	
+		mvprintw(y, x, "@");
+	}
+
 	getch();
 	endwin();
 	return 0;
